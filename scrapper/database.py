fc719073c9991ebf486e1bb42d518836ddc625be
João Vieira <joaofranciscovrfilho@gmail.com>
2023-07-08 18:19:12 -0300

    Removed unnecessary imports

--- scrapper/database.py
+++ scrapper/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
